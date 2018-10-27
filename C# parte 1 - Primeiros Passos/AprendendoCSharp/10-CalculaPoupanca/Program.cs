fa743fc40333ae4a2d61d44e06ee26d065394676
felipepatente <felipepatente@yahoo.com.br>
2018-10-27 20:33:44 -0300

    Laço de repetição For

---
+++ C# parte 1 - Primeiros Passos/AprendendoCSharp/10-CalculaPoupanca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 10 - Calcula Poupança");
+
+            double valorInvestido = 1000;
+            int contadoresMes = 1;
+
+            while (contadoresMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após "+ contadoresMes +" meses, você terá R$ " + valorInvestido);
+                contadoresMes++;
+            }
+            
+
+        }
+    }
+}
