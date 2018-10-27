b478be803fed0501bbd8c7aaad7948efa3829e78
felipepatente <felipepatente@yahoo.com.br>
2018-10-27 18:25:25 -0300

    Caracteres e textos

---
+++ C# parte 1 - Primeiros Passos/AprendendoCSharp/CaracteresETextos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            char primeiraLetra = ' ';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra += (char) 1;
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia " + 2020;
+            Console.WriteLine(titulo);
+
+            string cursosProgramacao = 
+@"- .NET
+- Java
+- JavaScript";
+
+            Console.WriteLine(cursosProgramacao);
+
+        }
+    }
+}
