4c3660ac03a33eec58a5b7531796e32077dbfef6
felipepatente <felipepatente@yahoo.com.br>
2018-10-27 19:40:05 -0300

    Escopo

--- C# parte 1 - Primeiros Passos/AprendendoCSharp/9-Escopo/Program.cs
+++ C# parte 1 - Primeiros Passos/AprendendoCSharp/9-Escopo/Program.cs
@@ -4,29 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _8_Condicionais2
+namespace _9_Escopo
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Executando projeto 8 - Condicionais 2");
+            Console.WriteLine("Executando projeto 9 - Escopo");
 
             int idadeJoao = 19;
             int quantidadePessoas = 2;
+            string mensagemAdicional;
 
             bool acompanhado = quantidadePessoas >= 2;
 
+            if (acompanhado)
+            {
+                mensagemAdicional = "João está acompanhado!";
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado!";
+            }
+            
             if (idadeJoao >= 18 || acompanhado)
             {
-                Console.WriteLine("Pode entrar");                             
+                Console.WriteLine("Pode entrar");
+                Console.WriteLine(mensagemAdicional);
             }
             else
             {
                 Console.WriteLine("Não pode entrar");
             }
-            
         }
     }
 }
