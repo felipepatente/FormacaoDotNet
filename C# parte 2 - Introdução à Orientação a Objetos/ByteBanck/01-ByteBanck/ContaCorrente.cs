3b5b6612624d784781803ac6b3811ea04ee016b4
felipepatente <felipepatente@yahoo.com.br>
2018-10-29 20:38:03 -0300

    Nossa primeira Classe

---
+++ C# parte 2 - Introdução à Orientação a Objetos/ByteBanck/01-ByteBanck/ContaCorrente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBanck
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int numero;
+        public double saldo;
+
+        public ContaCorrente()
+        {
+
+        }
+
+    }
+}
