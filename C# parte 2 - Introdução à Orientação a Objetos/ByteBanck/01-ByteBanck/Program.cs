968e68319960b40943d76efb76010359983f8ca8
felipepatente <felipepatente@yahoo.com.br>
2018-10-29 21:00:22 -0300

    Usando a nossa classe

--- C# parte 2 - Introdução à Orientação a Objetos/ByteBanck/01-ByteBanck/Program.cs
+++ C# parte 2 - Introdução à Orientação a Objetos/ByteBanck/01-ByteBanck/Program.cs
@@ -10,6 +10,21 @@ namespace _01_ByteBanck
     {
         static void Main(string[] args)
         {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
         }
     }
 }
