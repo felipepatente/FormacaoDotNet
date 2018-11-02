3a1aceffb5ab9fc807abc961a4573190aff2abe3
felipepatente <felipepatente@yahoo.com.br>
2018-11-02 15:50:02 -0300

    Construtores

---
+++ C# parte 2 - Introdução à Orientação a Objetos/ByteBanck/07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,89 @@
+﻿using _07_ByteBank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBanck
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        private int _agencia;        
+        public int Numero { get; set; }
+        private double _saldo = 100;
+        
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _saldo = value;
+                }
+            }
+        }
+        
+        public int Agencia {
+
+            get
+            {
+                return _agencia;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+
+                this._agencia = value;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }else
+            {
+                this._saldo -= valor;
+                contaDestino.Depositar(valor);
+
+                return true;
+            }
+        }
+
+    }
+}
