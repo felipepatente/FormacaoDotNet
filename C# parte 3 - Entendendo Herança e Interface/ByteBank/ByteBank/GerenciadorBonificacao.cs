22e2b78a610cdd06fd2c291adeb426bfd143f78e
felipepatente <felipepatente@yahoo.com.br>
2018-11-02 17:21:37 -0300

    Sobrecargas

---
+++ C# parte 3 - Entendendo Herança e Interface/ByteBank/ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,29 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificiacao();
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            _totalBonificacao += diretor.GetBonificiacao();
+        }
+
+        public double GetTotalBonificiacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
---
+++ C# parte 3 - Entendendo Herança e Interface/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "454.658.148-3";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificiacao());
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.Salario);
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificiacao());
+        }
+    }
+}
