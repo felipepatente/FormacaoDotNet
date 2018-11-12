b00dae446c92306cf5760f10827dd97fefb3be9d
felipepatente <felipepatente@yahoo.com.br>
2018-11-12 21:22:00 -0200

    Blocos Try Catch

---
+++ C# parte 4 - Entendendo Exceções/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Metodo();
+            }
+            catch (NullReferenceException erro)
+            {
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Aconteceu um erro!");
+            }
+            
+        }
+
+        private static void Metodo()
+        {
+            TestaDivisao(0);            
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é de " + resultado);
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Não é possível fazer uma divisão por 0");
+            }
+            
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+
+            //Console.WriteLine(conta.Saldo);
+            return numero / divisor;
+        }
+    }
+}
