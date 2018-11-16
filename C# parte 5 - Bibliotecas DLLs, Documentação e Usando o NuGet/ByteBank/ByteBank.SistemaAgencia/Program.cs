dc2495c8c9dab8d4f894294c271760827811b96c
felipepatente <felipepatente@yahoo.com.br>
2018-11-16 08:18:59 -0200

    Modificador de Acesso internal

--- C# parte 5 - Bibliotecas DLLs, Documentação e Usando o NuGet/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ C# parte 5 - Bibliotecas DLLs, Documentação e Usando o NuGet/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,7 +13,7 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
             ContaCorrente conta = new ContaCorrente(847,489754);
-
+            
             Console.WriteLine(conta.Numero);  
         }
     }
