221bbd170652e8e2d8ca98ad4eebc8f859b07a68
felipepatente <felipepatente@yahoo.com.br>
2018-11-16 15:54:06 -0200

    ToLower e ToUpper

--- C# parte 6 - String, Expressões Regulares e a Classe Object/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ C# parte 6 - String, Expressões Regulares e a Classe Object/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -13,7 +13,7 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
             
-            string urlParametros = "http:www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+            string urlParametros = "http:www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
             ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
 
             string valor = extratorDeValores.GetValor("moedaDestino");
@@ -22,14 +22,8 @@ namespace ByteBank.SistemaAgencia
             string palavra = "moedaOrigem=moedaDestino&moedaDestino=dolar";
             string nomeArgumento = "moedaDestino=";
             int indice = palavra.IndexOf(nomeArgumento);
-            Console.WriteLine(indice);
-            Console.WriteLine("Tamanho da String nomeArgumento" + nomeArgumento.Length);
-            Console.WriteLine(palavra.Substring(indice));
-            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length));
 
-            string testeRemocao = "primeiraParte&parteParRemover";
-            int indiceEComercial = testeRemocao.IndexOf("&");
-            Console.WriteLine(testeRemocao.Remove(indiceEComercial,4));
+            Console.WriteLine(extratorDeValores.GetValor("VALOR"));
             
         }
     }
