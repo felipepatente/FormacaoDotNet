f8ea288765e4024a64693384b3e189a5945e29a3
felipepatente <felipepatente@yahoo.com.br>
2018-11-16 14:33:36 -0200

    IsNullOrEmpty e palavra chave string

---
+++ C# parte 6 - String, Expressões Regulares e a Classe Object/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pagina?argumentos
+            // 012345678
+
+            string textoVazio = "";
+            string textoNulo = null;
+
+            Console.WriteLine(string.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(string.IsNullOrEmpty(textoNulo));
+
+            string url = "paginas?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf("?");
+
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(indiceInterrogacao + 1);
+            Console.WriteLine(argumentos);
+            
+        }
+    }
+
+
+}
