5056a4b3a189ade4765c2903fb2656058dcede2d
felipepatente <felipepatente@yahoo.com.br>
2018-11-16 18:06:01 -0200

    Método Equals

--- C# parte 6 - String, Expressões Regulares e a Classe Object/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ C# parte 6 - String, Expressões Regulares e a Classe Object/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -14,6 +14,26 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
 
+            Cliente carlos = new Cliente();
+            carlos.Nome = "Carlos";
+            carlos.CPF = "145.336.212-23";
+            carlos.Profissao = "Design";
+
+            Cliente carlos2 = new Cliente();
+            carlos2.Nome = "Carlos";
+            carlos2.CPF = "145.336.212-23";
+            carlos2.Profissao = "Design";
+
+            ContaCorrente conta3 = new ContaCorrente(25, 362);
+
+            if (carlos.Equals(conta3))
+            {
+                Console.WriteLine("São iguais");
+            }else
+            {
+                Console.WriteLine("Não são iguais");
+            }
+
 
             //string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";            
             //string padrao = "[0-9]{4,5}[-][0-9]{4}";            
@@ -29,13 +49,7 @@ namespace ByteBank.SistemaAgencia
 
             //Console.WriteLine(Regex.IsMatch(textoDeTeste, padrao));            
             Console.WriteLine(resultado.Value);
-
-
-
-
-
-
-
+            
             string urlParametros = "http:www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
             ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
 
