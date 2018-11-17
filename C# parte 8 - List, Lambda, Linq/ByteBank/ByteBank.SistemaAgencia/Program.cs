59f34336d2e42e2a3e2d47767e15dcf85ac1cde3
felipepatente <felipepatente@yahoo.com.br>
2018-11-17 16:50:14 -0200

    Ordenando Listas

--- C# parte 8 - List, Lambda, Linq/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ C# parte 8 - List, Lambda, Linq/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -13,23 +13,44 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            var contas = new List<ContaCorrente>()
+            {
+                new ContaCorrente(341,5254),
+                new ContaCorrente(342,5254),
+                new ContaCorrente(324,5254),
+                new ContaCorrente(314,5214)                
+            };
 
+            contas.Sort();
 
-            var conta = new ContaCorrente(344, 45582);
-            var gerenciador = new GerenciadorBonificacao();
-            var gerenciadores = new List<GerenciadorBonificacao>();
+            foreach (var conta in contas)
+            {
+                Console.WriteLine($"Conta número {conta.Numero}, ag. {conta.Agencia}");
+            }
 
-            conta.Depositar(2514);
+        }   
+        
+        static void TestarSort()
+        {
+            var nomes = new List<string>()
+            {
+                "Yasmin",
+                "Felipe",
+                "Juliana",
+                "Leticia",
+                "Jéssica",
+                "Monique",
+                "Aline"
+            };
 
+            nomes.Sort();
 
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
 
-
-
-
-
-
-
-            List<int> idades = new List<int>();
+            var idades = new List<int>();
 
             idades.Add(1);
             idades.Add(5);
@@ -38,13 +59,14 @@ namespace ByteBank.SistemaAgencia
             idades.Add(38);
             idades.Add(61);
 
-            idades.AdicionarVarios(45,36,25,14);
+            idades.AdicionarVarios(45, 36, 25, 14);
+
+            idades.Sort();
 
             for (int i = 0; i < idades.Count; i++)
-            {                
+            {
                 Console.WriteLine(idades[i]);
             }
-            
-        }        
+        }     
     }
 }
