4f212dc26698a098cb2194494514b7f96a260e39
felipepatente <felipepatente@yahoo.com.br>
2018-11-17 17:26:17 -0200

    Melhorando a comparação

--- C# parte 8 - List, Lambda, Linq/ByteBank/ByteBank.SistemaAgencia/Comparadores/ComparadorContaCorrentePorAgencia.cs
+++ C# parte 8 - List, Lambda, Linq/ByteBank/ByteBank.SistemaAgencia/Comparadores/ComparadorContaCorrentePorAgencia.cs
@@ -25,18 +25,9 @@ namespace ByteBank.SistemaAgencia.Comparadores
             {
                 return -1;
             }
+
+            return x.Agencia.CompareTo(y.Agencia);
             
-            if (x.Agencia < y.Agencia)
-            {
-                return -1;
-            }
-
-            if (x.Agencia == y.Agencia)
-            {
-                return 0;
-            }
-
-            return 1;
         }
     }
 }
