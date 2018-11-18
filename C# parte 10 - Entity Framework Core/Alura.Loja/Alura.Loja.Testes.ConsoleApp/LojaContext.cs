9e6edb5c10812bd2ad2f36f1496834a8fcbf3e3d
felipepatente <felipepatente@yahoo.com.br>
2018-11-18 14:54:00 -0200

    Primeiro uso da Ferramenta

---
+++ C# parte 10 - Entity Framework Core/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+                     
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("Server=DESKTOP-9D47OLL; Database=LojaDB;Trusted_Connection=true;");
+        }
+    }
+}
