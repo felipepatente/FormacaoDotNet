353db84dd9b077d012af6547f500813c53b806e0
felipepatente <felipepatente@yahoo.com.br>
2018-11-18 08:46:29 -0200

    Criando um FileStream

--- C# parte 9 - Entrada e Saída (I-O) com Streams/ByteBank/ByteBankImportacaoExportacao/Program.cs
+++ C# parte 9 - Entrada e Saída (I-O) com Streams/ByteBank/ByteBankImportacaoExportacao/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,48 @@ namespace ByteBankImportacaoExportacao
 { 
     class Program 
     { 
-        static void Main(string[] args) 
+        static void Main(string[] args)             
         {
-            Console.ReadLine();
+            string enderecoDoArquivo = "contas.txt";
+            FileStream fluxoDoArquivo = new FileStream(enderecoDoArquivo,FileMode.Open);
+
+            byte[] buffer = new byte[1024]; // 1kb
+            int numeroDeBytesLidos = -1;
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);                
+            }
+
+            EscreverBuffer(buffer);
         }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (buffer[i] != 0)
+                {
+                    Console.Write(buffer[i]);
+                    Console.Write(" ");
+                }
+            }
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 } 
  
