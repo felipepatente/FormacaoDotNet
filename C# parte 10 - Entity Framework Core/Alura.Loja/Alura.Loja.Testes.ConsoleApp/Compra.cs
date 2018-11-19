d57e34e56716411a18e31aa04930c7a7bda60ebd
felipepatente <felipepatente@yahoo.com.br>
2018-11-19 21:27:33 -0200

    Relacionamentos um para muitos

---
+++ C# parte 10 - Entity Framework Core/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Compra.cs
@@ -0,0 +1,13 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class Compra
+    {
+        public Compra()
+        {
+        }
+
+        public double Preco { get; internal set; }
+        public Produto Produto { get; internal set; }
+        public int Quantidade { get; internal set; }
+    }
+}
---
+++ C# parte 10 - Entity Framework Core/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paoFrances = new Produto();
+            paoFrances.Nome = "Pão Francês";
+            paoFrances.PrecoUnitario = 0.40;
+            paoFrances.Unidade = "Unidade";
+            paoFrances.Categoria = "Padaria";
+
+            var compra = new Compra();
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+        }               
+    }
+}
