55c32ff560088615492a69fbfa9ffdc413c8983a
felipepatente <felipepatente@yahoo.com.br>
2018-11-28 20:15:54 -0200

    O Padrão MVC

---
+++ C# parte 11 - ASP.NET Core - Uma Webapp usando o Padrão MVC/Alura.ListaLeitura/Alura.ListaLeitura.App/HTML/HtmlUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Alura.ListaLeitura.App.HTML
+{
+    public class HtmlUtils
+    {
+        public static string CarregaArquivoHTML(string nomeArquivo)
+        {
+            using (var arquivo = File.OpenText($"{nomeArquivo}.html"))
+            {
+                return arquivo.ReadToEnd();
+            }
+        }
+    }
+}
